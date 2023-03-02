276747859f10c3a8d3c04ee04ab489c9eb919a0e
Sam Diener <dienersh@mail.uc.edu>
2023-03-02 18:17:27 -0500

    fixed scaling

--- Visual Reality/Assets/ScaleObject.cs
+++ Visual Reality/Assets/ScaleObject.cs
@@ -10,6 +10,11 @@ public class ScaleObject : MonoBehaviour
     public GameObject leftHand;
     public GameObject rightHand;
 
+    private Vector3 lastPosL;
+    private Vector3 currPosL;
+    private Vector3 lastPosR;
+    private Vector3 currPosR;
+
     public InputActionProperty leftGrab;
     public InputActionProperty rightGrab;
     public InputActionProperty leftScale;
@@ -17,17 +22,23 @@ public class ScaleObject : MonoBehaviour
 
     
     void Start(){
-
+        currPosL = leftHand.transform.localPosition;
+        currPosR = rightHand.transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
+        lastPosL = currPosL;
+        currPosL = leftHand.transform.localPosition;
+        lastPosR = currPosR;
+        currPosR = rightHand.transform.localPosition;
+        
         if((leftGrab.action.ReadValue<float>()>0.1f && leftScale.action.ReadValue<float>()>0.1f)){
-            object2Scale.transform.localScale += leftHand.transform.localPosition;
+            object2Scale.transform.localScale += (currPosL - lastPosL);
         }
         if((rightGrab.action.ReadValue<float>()>0.1f && rightScale.action.ReadValue<float>()>0.1f)){
-            object2Scale.transform.localScale += rightHand.transform.localPosition;;
+            object2Scale.transform.localScale += (currPosR - lastPosR);
         }
     }
 }
