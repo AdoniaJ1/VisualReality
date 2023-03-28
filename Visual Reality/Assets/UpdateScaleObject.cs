abcd6aa32509ed2e7eab27d78f7bf04eabf59545
Nicholas Bowling <46771228+bowlinnr@users.noreply.github.com>
2023-03-28 16:09:44 -0400

    Working on Scaling Multiplier
    Not currently functional.  Messing with Scaling Script

--- Visual Reality/Assets/UpdateScaleObject.cs
+++ Visual Reality/Assets/UpdateScaleObject.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 
+
 public class UpdateScaleObject : MonoBehaviour
 {
     public GameObject targetL;
     public GameObject targetR;
     public InputActionProperty leftSelect;
     public InputActionProperty rightSelect;
-    private Vector3 lastPos;
-    private Vector3 currentPos;
+    private Vector3 lastPos = new Vector3();
+    private Vector3 currentPos = new Vector3();
     public bool grabL; 
+    //private Vector3 multi = new Vector3(1.5f,1.5f,1.5f);
+    private Vector3 difference = new Vector3();
     
 
     // Start is called before the first frame update
@@ -20,46 +23,54 @@ public class UpdateScaleObject : MonoBehaviour
     {
 
         if(leftSelect.action.ReadValue<float>() > .1f){
-            Vector3 lastPos = targetL.transform.localPosition;
-            grabL = true;
+             lastPos = targetL.transform.position;
+             grabL = true;
         } else {
-             Vector3 lastPos = targetR.transform.localPosition;
+             lastPos = targetR.transform.position;
              grabL = false;
         }
+        Debug.Log("Right Controller Position: "+  targetR.transform.position);
+        Debug.Log("Initial Position: "+ lastPos);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         if(grabL){
-            currentPos = targetL.transform.localPosition;
+            currentPos = targetL.transform.position;
+            Debug.Log("Current Position "+ currentPos);
+            Debug.Log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-            Debug.Log("Local Scale: "+ transform.localScale);
-            Debug.Log("+");
-            Debug.Log("Current Position R: "+ currentPos);
-            Debug.Log("-");
-            Debug.Log("Last Position R: "+ lastPos);
-            Debug.Log("=");
+            // Debug.Log("Local Scale: "+ transform.localScale);
+            // Debug.Log("+");
+            // Debug.Log("Current Position R: "+ currentPos);
+            // Debug.Log("-");
+            // Debug.Log("Last Position R: "+ lastPos);
+            // Debug.Log("=");
+            
+            //public static Vector3 operator *(1.5f, Vector3 a);
+            difference = (currentPos - lastPos);
+            transform.localScale += (difference*multi);
 
-            transform.localScale += (currentPos - lastPos);
-
-            Debug.Log("Local Scale: "+ transform.localScale);
-            Debug.Log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            // Debug.Log("Local Scale: "+ transform.localScale);
+            // Debug.Log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             lastPos = currentPos;
         } else {
-            currentPos = targetR.transform.localPosition;
+            currentPos = targetR.transform.position;
+            Debug.Log("Current Position "+ currentPos);
+            Debug.Log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-            Debug.Log("Local Scale: "+ transform.localScale);
-            Debug.Log("+");
-            Debug.Log("Current Position R: "+ currentPos);
-            Debug.Log("-");
-            Debug.Log("Last Position R: "+ lastPos);
-            Debug.Log("=");
+            // Debug.Log("Local Scale: "+ transform.localScale);
+            // Debug.Log("+");
+            // Debug.Log("Current Position R: "+ currentPos);
+            // Debug.Log("-");
+            // Debug.Log("Last Position R: "+ lastPos);
+            // Debug.Log("=");
+            difference = (currentPos - lastPos);
+            transform.localScale += (difference * multi);
 
-            transform.localScale += (currentPos - lastPos);
-
-            Debug.Log("Local Scale: "+ transform.localScale);
-            Debug.Log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            //Debug.Log("Local Scale: "+ transform.localScale);
+            //Debug.Log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             lastPos = currentPos;
         }
     }
