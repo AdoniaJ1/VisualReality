9c1e6943ed5e3bd16ed42d774f8907ec46af8f78
AdoniaJ <jebessah@mail.uc.edu>
2023-04-02 17:47:33 -0400

    wrist menu initial dropdown test

--- Visual Reality/Assets/SpawnerScript.cs
+++ Visual Reality/Assets/SpawnerScript.cs
@@ -2,19 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class SpawnerScript : MonoBehaviour
 {
 
     public InputActionProperty rightSpecial;
+    public TMPro.TMP_Dropdown shapeDropDown;
 
+    public GameObject shapePrefab;
     public GameObject cubePrefab;
+    public GameObject spherePrefab;
+
     void Update()
     {
         if(rightSpecial.action.triggered)
         {
-            Instantiate(cubePrefab, transform.position, Quaternion.identity);
+            if(shapeDropDown.value == 0) shapePrefab = cubePrefab;
+            if(shapeDropDown.value == 1) shapePrefab = spherePrefab;
+
+            Instantiate(shapePrefab, transform.position, Quaternion.identity);
         }
     }
 }
