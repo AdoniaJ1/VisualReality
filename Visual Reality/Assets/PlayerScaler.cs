d1a03286be487467666fa1b78fa563bcd1357304
Nicholas Bowling <46771228+bowlinnr@users.noreply.github.com>
2023-04-03 17:45:38 -0400

    Scale Caps
    Added hi low scale caps

--- Visual Reality/Assets/PlayerScaler.cs
+++ Visual Reality/Assets/PlayerScaler.cs
@@ -7,16 +7,26 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlayerScaler : MonoBehaviour
 {
+    public Vector3 size = new Vector3 (.2f,.2f,.2f);
+    public Vector3 jump = new Vector3 (0f, 2.5f, 0f);
+    float limitLo = .6f;
+    float limitHi = 1.6f;
     // Start is called before the first frame update
     public void PlayerIncrementer()
     {
-        transform.localScale += Vector3.one;
+        if(transform.localScale.x < limitHi){
+            transform.localScale += size;
+            transform.localPosition += jump;
+        }
     }
 
     // Update is called once per frame
-    public void PlayerDecrementer()
+    public void PlayerDecrementer() 
     {
-        transform.localScale -= Vector3.one;
+        if(transform.localScale.x > limitLo){
+            transform.localScale -= size;
+            transform.localPosition += jump;
+        }
     }
 
 }
