a6c30c3865d0d1a8bbaeb83457ffee9ca8b5042a
Nicholas Bowling <46771228+bowlinnr@users.noreply.github.com>
2023-04-05 21:36:44 -0400

    scene change
    add button to scene change

---
+++ Visual Reality/Assets/DropDown.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DropDown : MonoBehaviour
+{
+
+    public TMPro.TMP_Dropdown sceneDrop;
+
+    // Start is called before the first frame update
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+            }
+
+   
+   public void getDropdown()
+    {
+        if (sceneDrop.value ==0) {LoadScene("Main Scene");}
+          if (sceneDrop.value ==1) {LoadScene("Test Scene NRB");}
+    }
+
+    public void quit() {
+        Application.Quit();
+
+    }
+}
